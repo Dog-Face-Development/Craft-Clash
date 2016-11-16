cb19bee2a5c6a864fc613753eb3d3dd6786d6a00
William <willtheorangeguy@users.noreply.github.com>
2016-11-15 16:51:16 -0800

    Upload repo files.

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
+## Copyright (C) 2016 Necti Co. and Dog Face Development Co.
+## All rights reserved.
 """
-CRAFT-CLASH
+CRAFT CLASH
 Main project file.
 """
-import tkinter
+# Import Statements
+from tkinter import *
 import turtle
-import craft-clash.playscreen
+from craftclash.playscreen import *
+from craftclash.optionscreen import *
+
+# Window Elements
+window = Tk()
+window.title("Craft Clash - Windows - 0.0.1 BETA")
+window.configure(bg = "sky blue")
+#if "nt" == os.name:
+#    window.wm_iconbitmap(bitmap = "assets/logo/titletumbnail.gif")
+
+# Images
+titleimg = PhotoImage(file = "assets/logo/titlelogo.gif")
+
+#Widgets
+titlelabel = Label(window, image = titleimg)
+btn_play = Button(window, text = "Play!", height = 3, width = 60, bd = 4, relief = RAISED, command = playscreen)
+btn_options = Button(window, text = "Options", height = 3, width = 60, bd = 4, relief = RAISED, command = exit)
+btn_exit = Button(window, text = "Exit", height = 3, width = 60, bd = 4, relief = RAISED, command = exit)
+copyright_label = Label(window, text = "Copyright (C) 2016 Necti Co. and Dog Face Development Co. \t\t\t\t Version 0.0.1 BETA")
+
+# Pack Statements
+titlelabel.pack(side = TOP, pady = 20)
+btn_play.pack(side = TOP, padx = 300, pady = 5)
+btn_options.pack(side = TOP, padx = 300, pady = 5)
+btn_exit.pack(side = TOP, padx = 300, pady = 5)
+copyright_label.pack(side = BOTTOM, padx = 100, pady = 50)
+
+# Sustain Window
+window.mainloop()
