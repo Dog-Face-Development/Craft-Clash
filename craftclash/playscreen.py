b13a2710fa5244672a245922fd1cc14e2eae53eb
William <willtheorangeguy@users.noreply.github.com>
2016-11-18 17:11:45 -0800

    Update spelling errors.

--- craftclash/playscreen.py
+++ craftclash/playscreen.py
@@ -16,9 +16,9 @@ def playscreen():
     window.title("Craft Clash - Windows - 0.0.1 BETA - Play!")
     window.resizable(0,0)
     # Widgets
-    worldoneimg = PhotoImage(file = "D:/William/Documents/Necti Co/CraftClash/BETA_V1.0/assets/logo/titlethumbnail.gif") # Change absolute path on you own computer to the assets/logo/titlethumbnail.gif, for more infor se the README.
+    worldoneimg = PhotoImage(file = "D:/William/Documents/Necti Co/CraftClash/BETA_V1.0/assets/logo/titlethumbnail.gif") # Change absolute path on you own computer to the assets/logo/titlethumbnail.gif, for more info see the README.
     btn_createnewworld = Button(window, text = "Create New World!", height = 2, width = 80, command = exit)
-    worldinfo = Label(window, text= "We have already created two worlds for you to play in. Chose your pick of world or play in both of them. \n You can create another world by clicking the 'Create New World!' button at the top of the screen") 
+    worldinfo = Label(window, text= "We have already created a world for you to play in.\n You can create another world by clicking the 'Create New World!' button at the top of the screen") 
     btn_worldone = Button(window, text = "World 1", height = 4, width = 50, command = exit)
     btn_worldoneimg = Button(window, image = worldoneimg)
     # Pack Statements
