ce6c2a3bf6739582173fc2cf4ebb56e30ec06807
Derpyface <william_vandergraaf@lcsstudent.ca>
2017-12-07 16:50:46 -0800

    Initial BETAv0.0.3 files and doc files

---
+++ setup_information.py
@@ -0,0 +1,4 @@
+setup( name = 'CraftClash',
+       version = '0.0.3 BETA',
+       description = 'Spend the day upgrading your buildings and walls, craft new items and tend to your world. Have fun spending the night fighting off monsters and hoping you and your world can endure the damage that they do! Written in Python 3 . Fully open source!!',
+       author = 'Derpyface Developement Co.' )
