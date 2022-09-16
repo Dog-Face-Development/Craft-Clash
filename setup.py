67d5d0c92ca08da0987a5594a83782556c3a7354
William <willtheorangeguy@users.noreply.github.com>
2022-09-15 23:04:38 -0700

    Add all community docs

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+	name='craftclash',
+	version='0.4.0',
+    description="A game that is a cross of Minecraft and Clash of Clans, written in Python!",
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Games/Entertainment',
+    ],
+    keywords='minecraft clash of clans royale game food items blocks defense attack battle gui',
+    url='https://github.com/Dog-Face-Development/Craft-Clash',
+    author='willtheorangeguy',
+    packages=find_packages(where="craftclash"),
+    package_dir={"": "craftclash"},
+    include_package_data=True,
+    py_modules = ['main', 'aboutscreen', 'optionsscreen', 'playscreen.old', 'craftclash/aboutscreen', 'craftclash/optionsscreen', 'craftclash/playscreen.old'],
+    entry_points={
+        'console_scripts': [
+            'craftclash=main:craftclash'
+        ]
+    },
+)
