eca04be39c956dc50a5a1b5d59492f21412b54e2
William Vandergraaf <willtheorangeguy@users.noreply.github.com>
2023-12-06 20:23:44 -0700

    Update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,60 @@
-"""Test the main.py file."""
-#pylint: disable=import-error, invalid-name
+"""Test the main application."""
+#pylint: disable=import-error, wrong-import-position
 
-# Get out of the test directory
 import os
 import sys
 import unittest
+from tkinter import Label, Button
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import craftclash
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(os.path.join(parent_dir))
-
-
-class TestCraftClash(unittest.TestCase):
-    """Test the CraftClash class in main.py."""
-
+class TestApp(unittest.TestCase):
+    """Test the main application."""
     def setUp(self):
-        """Set up the test."""
+        """Create the application."""
         self.app = craftclash()
 
-    def test_window_title(self):
-        """Test the window title."""
-        self.assertEqual(self.app.title(), "CraftClash - Windows - 0.0.4 BETA")
+    def tearDown(self):
+        """Destroy the application."""
+        self.app.window.destroy()
 
+    def test_window_exists(self):
+        """Test if the window exists."""
+        self.assertIsNotNone(self.app)
 
-if __name__ == "__main__":
+    def test_window_background(self):
+        """Test the window background color."""
+        self.assertEqual(self.app.cget('bg'), 'sky blue')
+
+    def test_title_label_exists(self):
+        """Test if the title label exists."""
+        title_label = self.app.grid_slaves(row=1, column=2)[0]
+        self.assertIsInstance(title_label, Label)
+
+    def test_play_button_exists(self):
+        """Test if the play button exists."""
+        btn_play = self.app.grid_slaves(row=3, column=2)[0]
+        self.assertIsInstance(btn_play, Button)
+        self.assertEqual(btn_play.cget('text'), 'Play!')
+
+    def test_options_button_exists(self):
+        """Test if the options button exists."""
+        btn_options = self.app.grid_slaves(row=4, column=2)[0]
+        self.assertIsInstance(btn_options, Button)
+        self.assertEqual(btn_options.cget('text'), 'Options')
+
+    def test_about_button_exists(self):
+        """Test if the about button exists."""
+        btn_about = self.app.grid_slaves(row=5, column=2)[0]
+        self.assertIsInstance(btn_about, Button)
+        self.assertEqual(btn_about.cget('text'), 'About')
+
+    def test_copyright_label_exists(self):
+        """Test if the copyright label exists."""
+        copyright_label = self.app.grid_slaves(row=6, column=1)[0]
+        self.assertIsInstance(copyright_label, Label)
+
+if __name__ == '__main__':
     unittest.main()
